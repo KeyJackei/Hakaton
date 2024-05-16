d6238b5790c554e7a723c5dc1142ea898b7f3d3c
LisDemlord <lis.demlord@gmail.com>
2024-05-16 12:14:58 +0500

    testing

--- hakaton_ambulance/hakaton_ambulance/settings.py
+++ hakaton_ambulance/hakaton_ambulance/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # другие приложения
+    'hakaton_ambulance',
+    'ViewQueue',
 ]
 
 MIDDLEWARE = [
