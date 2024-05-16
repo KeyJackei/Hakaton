ae4d2a82bf02371e2edd7cdce758e2507f8e7c53
zoo11 <zoo1102@mail.ru>
2024-05-16 14:32:04 +0500

    Imba registration

---
+++ hakaton_ambulance/appointments/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from BaseTables.models import Cabinet
+
+
+def appointment_form(request):
+    specialists = Cabinet.objects.all()
+    return render(request, 'appointments/form.html', {'specialists': specialists})
