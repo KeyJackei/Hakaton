bb9d051680a0e0a462b8dbf00d95eb42c0514b82
zoo11 <zoo1102@mail.ru>
2024-05-16 14:10:48 +0500

    delete BaseTable

--- hakaton_ambulance/ViewQueue/migrations/0001_initial.py
+++ hakaton_ambulance/ViewQueue/migrations/0001_initial.py
@@ -14,9 +14,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id_queue', models.IntegerField(primary_key=True, serialize=False)),
                 ('date', models.DateField()),
-                ('cabinet_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues', to='BaseTables.cabinet')),
-                ('patient_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues', to='BaseTables.patient')),
-                ('time_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues', to='BaseTables.interval')),
+                ('cabinet_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues',
+                                                 to='BaseTables.cabinet')),
+                ('patient_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues',
+                                                 to='BaseTables.patient')),
+                ('time_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='queues',
+                                              to='BaseTables.interval')),
             ],
         ),
     ]
